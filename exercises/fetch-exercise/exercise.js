cb1ea8cbfaeb27d1e920124af27c115cd84969b9
Lorena <lorenacapraru@gmail.com>
2023-02-09 12:42:32 +0000

    Update exercise.js

--- exercises/fetch-exercise/exercise.js
+++ exercises/fetch-exercise/exercise.js
@@ -17,10 +17,12 @@ Open index.html in your browser. Every time you refresh the page,
 a different greeting should be displayed in the box.
 */
 
-fetch("*** Write the API address here ***")
+fetch("https://holy-snow-6805.fly.dev/api/greetings")
   .then(function (response) {
-    return response.text();
+    return response.json();
   })
   .then(function (greeting) {
-    // Write the code to display the greeting text here
+    let greetingText = document.getElementById("greeting-text");
+    greetingText.innerHTML =
+      "Language: " + greeting.language + " --> " + greeting.hello;
   });
